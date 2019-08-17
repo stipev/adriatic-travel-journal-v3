b81a649ce1afb1b1edeba6cdd1429a28f7ac4cd1
stipev <cupkou12@gmail.com>
2019-08-18 00:38:45 +0200

    Add places.json file with places and update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,109 +1,103 @@
 import React, { Component } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import greenMarker from "../assets/greenMarker.png";
+import blueMarker from "../assets/blueMarker.png";
+import * as places from "../data/adriaticCoast.json";
 
 class Home extends Component {
   state = {
     viewport: {
-      width: 400,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
+      width: 600,
+      height: 600,
+      latitude: 43.7416835,
+      longitude: 15.8174061,
+      zoom: 7,
+      selectedPlace: " "
     }
   };
 
+  setSelectedPlace(place) {
+    this.setState({ selectedPlace: place });
+  }
+
   render() {
+    console.log("places: ", places.places[0]);
     return (
-      <div>
+      <div style={{ marginTop: "100px" }}>
         <ReactMapGL
           {...this.state.viewport}
           mapboxApiAccessToken="pk.eyJ1Ijoic3RpcGUyMTEyIiwiYSI6ImNqejRpMjk4djBkOXAzbnBhYmhqNncxaWUifQ.Dt6V1FJWMsuz80YEx0ghdg"
           onViewportChange={viewport => this.setState({ viewport })}
           mapStyle="mapbox://styles/stipe2112/cjz4im98v00zq1cn20gbvl8qp"
-        />
+        >
+          {places.places.map(place => (
+            <Marker
+              key={place.id}
+              latitude={place.latitude}
+              longitude={place.longitude}
+            >
+              <button
+                style={{
+                  background: "none",
+                  border: "none",
+                  cursor: "pointer"
+                }}
+                className="marker-btn"
+                onClick={e => {
+                  e.preventDefault();
+                  if (e.key === "Escape") {
+                    this.setSelectedPlace(null);
+                  }
+                  this.setSelectedPlace(place);
+                }}
+              >
+                <img
+                  style={{ width: "40px", height: "40px" }}
+                  src={place.visited ? blueMarker : greenMarker}
+                  alt="Skate Park Icon"
+                />
+              </button>
+            </Marker>
+          ))}
+
+          {this.state.selectedPlace ? (
+            <Popup
+              latitude={this.state.selectedPlace.latitude}
+              longitude={this.state.selectedPlace.longitude}
+              onClick={() => {
+                console.log("POPUP");
+              }}
+              closeOnClick={false}
+              closeButton={true}
+              onClose={() => {
+                this.setSelectedPlace(null);
+              }}
+            >
+              <div>
+                <img
+                  style={{ width: "200px", height: "200px" }}
+                  src="./greenMarker.png"
+                  alt="Skate Park Icon"
+                />
+                <h2
+                  style={{
+                    textDecoration: "underline"
+                  }}
+                  onClick={() => {
+                    console.log("ACTION WILL HAPPEN");
+                  }}
+                >
+                  KLIKNI TU ZA AKCIJU
+                </h2>
+                <h2>{this.state.selectedPlace.Name}</h2>
+                <p>{this.state.selectedPlace.Description}</p>
+              </div>
+            </Popup>
+          ) : null}
+        </ReactMapGL>
       </div>
     );
   }
 }
 
 export default Home;
-
-// import React, { useState, useEffect } from "react";
-// import ReactMapGL, { Marker, Popup } from "react-map-gl";
-// import * as parkDate from "./data/skateboard-parks.json";
-
-// export default function App() {
-//   const [viewport, setViewport] = useState({
-//     // 43.517801,,13.75z
-
-//     latitude: 43.4211,
-//     longitude: 16.4606093,
-//     //width: "100vw",
-//     width: "40rem",
-//     height: "40rem",
-//     //height: "100vh",
-//     zoom: 7.5
-//   });
-//   const [selectedPark, setSelectedPark] = useState(null);
-
-//   useEffect(() => {
-//     const listener = e => {
-//       if (e.key === "Escape") {
-//         setSelectedPark(null);
-//       }
-//     };
-//     window.addEventListener("keydown", listener);
-
-//     return () => {
-//       window.removeEventListener("keydown", listener);
-//     };
-//   }, []);
-
-//   return (
-//     <div style={{ height: "40rem", width: "40rem", border: "3px solid red" }}>
-//       <ReactMapGL
-//         {...viewport}
-//         mapboxApiAccessToken="pk.eyJ1Ijoic3RpcGUyMTEyIiwiYSI6ImNqejRpMjk4djBkOXAzbnBhYmhqNncxaWUifQ.Dt6V1FJWMsuz80YEx0ghdg"
-//         //{process.env.REACT_APP_MAPBOX_TOKEN}
-//         //mapStyle="mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4"
-//         mapStyle="mapbox://styles/stipe2112/cjz4im98v00zq1cn20gbvl8qp"
-//         onViewportChange={viewport => {
-//           setViewport(viewport);
-//         }}
-//       >
-//         {parkDate.features.map(park => (
-//           <Marker
-//             key={park.properties.PARK_ID}
-//             latitude={park.geometry.coordinates[1]}
-//             longitude={park.geometry.coordinates[0]}
-//           >
-//             <button
-//               className="marker-btn"
-//               onClick={e => {
-//                 e.preventDefault();
-//                 setSelectedPark(park);
-//               }}
-//             >
-//               <img src="/skateboarding.svg" alt="Skate Park Icon" />
-//             </button>
-//           </Marker>
-//         ))}
-
-//         {selectedPark ? (
-//           <Popup
-//             latitude={selectedPark.geometry.coordinates[1]}
-//             longitude={selectedPark.geometry.coordinates[0]}
-//             onClose={() => {
-//               setSelectedPark(null);
-//             }}
-//           >
-//             <div>
-//               <h2>{selectedPark.properties.NAME}</h2>
-//               <p>{selectedPark.properties.DESCRIPTIO}</p>
-//             </div>
-//           </Popup>
-//         ) : null}
-//       </ReactMapGL>
-//     </div>
-//   );
-// }
